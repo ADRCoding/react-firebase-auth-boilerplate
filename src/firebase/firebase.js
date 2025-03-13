967bfc4a377548e2ac117b6f25e1d0584146d6cd
MINECRAFTPVP889 <arushdineshraja18@gmail.com>
2025-03-12 22:22:48 -0500

    first commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyCFcUuuglAj0nsBakJifVHlkzqKsuktdc8",
+  authDomain: "collegebites-eb1a0.firebaseapp.com",
+  projectId: "collegebites-eb1a0",
+  storageBucket: "collegebites-eb1a0.firebasestorage.app",
+  messagingSenderId: "1085974052839",
+  appId: "1:1085974052839:web:0b197211918bdce33a0742",
+  measurementId: "G-YWWBQXVZ6N"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
