8f3b6e68ce92b991cef6192f29cdfc4f74e4349e
MINECRAFTPVP889 <arushdineshraja18@gmail.com>
2025-03-13 21:27:16 -0500

    signup

--- src/components/auth/register/index.jsx
+++ src/components/auth/register/index.jsx
@@ -4,12 +4,11 @@ import { useAuth } from '../../../contexts/authContext'
 import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth'
 
 const Register = () => {
-
     const navigate = useNavigate()
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [confirmPassword, setconfirmPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
     const [isRegistering, setIsRegistering] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
 
@@ -17,7 +16,7 @@ const Register = () => {
 
     const onSubmit = async (e) => {
         e.preventDefault()
-        if(!isRegistering) {
+        if (!isRegistering) {
             setIsRegistering(true)
             await doCreateUserWithEmailAndPassword(email, password)
         }
@@ -27,77 +26,83 @@ const Register = () => {
         <>
             {userLoggedIn && (<Navigate to={'/home'} replace={true} />)}
 
-            <main className="w-full h-screen flex self-center place-content-center place-items-center">
-                <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
-                    <div className="text-center mb-6">
-                        <div className="mt-2">
-                            <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
-                        </div>
-
+            <div className="bg">
+                <div className="login">
+                    <div className="loginTxt">
+                        Create a New Account
                     </div>
-                    <form
-                        onSubmit={onSubmit}
-                        className="space-y-4"
-                    >
-                        <div>
+                    <form onSubmit={onSubmit} className="space-y-4">
+                        {/* Email input */}
+                        <div className="inputContainer">
                             <label className="text-sm text-gray-600 font-bold">
                                 Email
                             </label>
                             <input
                                 type="email"
-                                autoComplete='email'
+                                autoComplete="email"
                                 required
-                                value={email} onChange={(e) => { setEmail(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="inputField"
+                                placeholder="Enter your email"
                             />
                         </div>
 
-                        <div>
+                        {/* Password input */}
+                        <div className="inputContainer">
                             <label className="text-sm text-gray-600 font-bold">
                                 Password
                             </label>
                             <input
                                 disabled={isRegistering}
                                 type="password"
-                                autoComplete='new-password'
+                                autoComplete="new-password"
                                 required
-                                value={password} onChange={(e) => { setPassword(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+                                value={password} 
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="inputField"
+                                placeholder="Enter your password"
                             />
                         </div>
 
-                        <div>
+                        {/* Confirm Password input */}
+                        <div className="inputContainer">
                             <label className="text-sm text-gray-600 font-bold">
                                 Confirm Password
                             </label>
                             <input
                                 disabled={isRegistering}
                                 type="password"
-                                autoComplete='off'
+                                autoComplete="off"
                                 required
-                                value={confirmPassword} onChange={(e) => { setconfirmPassword(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+                                value={confirmPassword} 
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                className="inputField"
+                                placeholder="Confirm your password"
                             />
                         </div>
 
+                        {/* Error message */}
                         {errorMessage && (
-                            <span className='text-red-600 font-bold'>{errorMessage}</span>
+                            <span className="text-red-600 font-bold">{errorMessage}</span>
                         )}
 
+                        {/* Sign Up Button */}
                         <button
                             type="submit"
                             disabled={isRegistering}
-                            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
+                            className={`loginBtn ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : ''}`}
                         >
                             {isRegistering ? 'Signing Up...' : 'Sign Up'}
                         </button>
-                        <div className="text-sm text-center">
-                            Already have an account? {'   '}
-                            <Link to={'/login'} className="text-center text-sm hover:underline font-bold">Continue</Link>
+
+                        {/* Login Link */}
+                        <div className="signup">
+                            Already have an account? <Link to={'/login'} className="font-bold hover:underline">Continue</Link>
                         </div>
                     </form>
                 </div>
-            </main>
+            </div>
         </>
     )
 }
